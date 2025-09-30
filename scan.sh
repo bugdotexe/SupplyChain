160a29354561d694938706fc025f701a24195d85
Nyein Chan Aung <nca0x93@gmail.com>
2025-09-30 18:30:38 +0630

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -22,7 +22,10 @@ LOWER=$(echo "${ORG}" | tr '[:upper:]' '[:lower:]')
 bash ghClone.sh $ORG
 bash main.sh $ORG "/tmp/$ORG/REPOS/${LOWER}"
 bash depChecker.sh $ORG "/tmp/$ORG"
-trufflehog github --only-verified --token=$GITHUB_TOKEN --issue-comments --pr-comments --gist-comments --include-members --archive-max-depth=150 --org=$ORG
+trufflehog github --only-verified --token=$GITHUB_TOKEN --issue-comments --pr-comments --gist-comments --include-members --archive-max-depth=50 --org=$ORG
+
+echo -e "${BLUE}Scanning Secrets from Javascripts $NAME ${NC}"
+find "/tmp/$ORG/REPOS/${LOWER}" -name "*.js" | xargs -I {} bash JsLeak.sh {} | sort -u | anew "/tmp/$ORG/secrets.potential"
 
 while IFS= read -r member; do
 bash main.sh ${member} "/tmp/$ORG/${member}/"
@@ -32,5 +35,7 @@ LOWER=$(echo "${member}" | tr '[:upper:]' '[:lower:]')
 for f in "/tmp/$ORG/${member}/REPOS/${LOWER}/*"; do
   [ -e "$f" ] || continue
   trufflehog git file://"/tmp/$ORG/${member}/REPOS/${LOWER}/${f##*/}" --only-verified --archive-max-depth=150
+  echo -e "${BLUE}Scanning Secrets from Javascripts $NAME ${NC}"
+find "/tmp/$ORG/${member}/REPOS/${LOWER}/" -name "*.js" | xargs -I {} bash JsLeak.sh {} | sort -u | anew "/tmp/$ORG/${member}/secrets.potential"
 done
 done < "/tmp/$ORG/member.usernames"
