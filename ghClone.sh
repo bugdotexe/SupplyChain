793e23cde7f6045157450eca52ee659dc5a1b5a6
Nyein Chan Aung <nca0x93@gmail.com>
2025-09-30 10:45:44 +0630

    Update and rename clone.sh to ghClone.sh

---
+++ ghClone.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+ORG=$1
+mkdir -p /tmp/$ORG/
+mkdir -p /tmp/$ORG/REPOS
+
+OUTPUT_DIR="/tmp/$ORG/REPOS/"
+ROOT_DIR="/tmp/$ORG/"
+
+org() {
+echo -e "${BLUE} Cloning repositories of${NC} ${RED}$ORG ${NC}"
+ghorg clone $ORG --fetch-all --quiet -p $OUTPUT_DIR -t $GITHUB_TOKEN --color enabled
+}
+
+member() {
+echo -e "${BLUE} Fetching GITHUB members of${NC} ${RED}$ORG ${NC}"
+bash findEmployees.sh $ORG
+
+while IFS= read -r NAME; do
+mkdir -p "$ROOT_DIR/$NAME"
+local OUTPUT_DIR="$ROOT_DIR/$NAME/REPOS"
+echo -e "${BLUE} Cloning repositories of${NC} ${RED}$NAME ${NC}"
+ghorg clone $NAME --clone-type=user --fetch-all --quiet -p $OUTPUT_DIR -t $GITHUB_TOKEN --color enabled
+done < $ROOT_DIR/member.usernames
+}
+
+org
+member
