041dd56c01839085e947ae7c468a3843bffb53cf
Nyein Chan Aung <nca0x93@gmail.com>
2025-10-07 00:09:57 +0630

    Update depChecker.sh

--- depChecker.sh
+++ depChecker.sh
@@ -9,35 +9,23 @@ NAME=$1
 OUT_PUT=$2
 
 echo -e "${BLUE}Checking NPM Dependencies $NAME ${NC}"
-while IFS= read -r pkg; do
-if npm view "$pkg" >/dev/null 2>&1; then
-    echo -e "${BLUE}[NPM]${NC} ${BLUE}$pkg${NC} ${BLUE}[AVAILABLE]${NC}"
-    echo "${pkg}" | anew $OUT_PUT/npm.potential >/dev/null 2>&1
-  fi
-done < "$OUT_PUT/npm.deps" | sed 's/[[:space:]]//g' | awk '{print $1}'
-
+cat "$OUT_PUT/npm.deps" | sed 's/[[:space:]]//g' | awk '{print $1}' | xargs -I {} npm-name {} | anew $OUT_PUT/npm.checked
+cat $OUT_PUT/npm.checked | grep "is available" | anew $OUT_PUT/npm.potential
 
 echo -e "${BLUE}Checking Python Dependencies $NAME ${NC}"
-while IFS= read -r pkg; do
-if pip show "$pkg" >/dev/null 2>&1; then
-    echo -e "${BLUE}[PIP]${NC} ${BLUE}$pkg${NC} ${BLUE}[AVAILABLE]${NC}"
-echo "${pkg}" | anew $OUT_PUT/pip.potential >/dev/null 2>&1
-  fi
-done < "$OUT_PUT/pip.deps" | sed 's/[[:space:]]//g' | awk '{print $1}'
-
-
+cat "$OUT_PUT/pip.deps" | sed 's/[[:space:]]//g' | awk '{print $1}' | xargs -I {} pip-name {} | anew $OUT_PUT/pip.checked
+cat $OUT_PUT/pip.checked | grep "is available" | anew $OUT_PUT/pip.potential
 
 echo -e "${BLUE}Checking Ruby Gem Dependencies $NAME ${NC}"
-while IFS= read -r pkg; do
+cat "$OUT_PUT/ruby.deps" | xargs -I {} ruby-name {} | anew $OUT_PUT/gem.checked
+cat $OUT_PUT/gem.checked | grep "is available" | anew $OUT_PUT/gem.potential
 
-if gem info "$pkg" >/dev/null 2>&1; then
-    echo -e "${BLUE}[GEM]${NC} ${BLUE}$pkg${NC} ${BLUE}[AVAILABLE]${NC}"
-   echo "${pkg}" | anew $OUT_PUT/ruby.potential >/dev/null 2>&1
- fi
-done < "$OUT_PUT/ruby.deps" | sed 's/[[:space:]]//g' | awk '{print $1}'
-
-broken() {
+broken_github() {
 echo -e "${BLUE}Scanning Broken GITHUB link $NAME ${NC}"
 cat $OUT_PUT/github.account | cut -d "/" -f1,2,3,4 | sort | uniq | xargs -I {} sh scan-broken.sh {} | anew $OUT_PUT/github.potential
+cat $OUT_PUT/github.action | cut -d "/" -f1,2,3,4 | sort | uniq | xargs -I {} sh scan-broken.sh {} | anew $OUT_PUT/github.potential
+
 #cat $OUT_PUT/github.account | grep -v ajax.googleapis.com\|awscli.amazonaws.com\|docs.aws.amazon.com\|ec2.amazonaws.com\|fonts.googleapis.com\|maps.googleapis.com\|oauth2.googleapis.com\|openidconnect.googleapis.com\|play.googleapis.com\|sns.amazonaws.com\|go-integration-test" | grep "actions-contrib\|googleapis\|amazonaws\|vercel.app\|netlify\|herokuapp\|surge.sh\|now.sh\|plugins.svn.wordpress.org\|npmjs.org\/package"
 }
+
+broken_github
