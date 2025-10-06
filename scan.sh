492a019a613e470d8e75a6f314055c8eb5e3db61
Nyein Chan Aung <nca0x93@gmail.com>
2025-10-07 00:11:20 +0630

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+RED="\e[31m"
+RESET="\e[0m"
+GREEN="\e[32m"
 
 echo
 echo -e "[+] World \e[31mOFF\e[0m,Terminal \e[32mON \e[0m"
-echo -e "                                 _____
-   ____   ____   ____   ____    /  |  |   ___________ ___.__.
-  / ___\ /  _ \ /    \ /    \  /   |  |__/ ___\_  __ <   |  |
- / /_/  >  <_> )   |  \   |  \/    ^   /\  \___|  | \/\___  |
- \___  / \____/|___|  /___|  /\____   |  \___  >__|   / ____|
-/_____/             \/     \/      |__|      \/       \/
-"
+echo -e " █████                             █████           █████
+░░███                             ░░███           ░░███
+ ░███████  █████ ████  ███████  ███████   ██████  ███████    ██████  █████ █████  ██████
+ ░███░░███░░███ ░███  ███░░███ ███░░███  ███░░███░░░███░    ███░░███░░███ ░░███  ███░░███
+ ░███ ░███ ░███ ░███ ░███ ░███░███ ░███ ░███ ░███  ░███    ░███████  ░░░█████░  ░███████
+ ░███ ░███ ░███ ░███ ░███ ░███░███ ░███ ░███ ░███  ░███ ███░███░░░    ███░░░███ ░███░░░
+ ████████  ░░████████░░███████░░████████░░██████   ░░█████ ░░██████  █████ █████░░██████
+░░░░░░░░    ░░░░░░░░  ░░░░░███ ░░░░░░░░  ░░░░░░     ░░░░░   ░░░░░░  ░░░░░ ░░░░░  ░░░░░░
+                      ███ ░███
+                     ░░██████
+                      ░░░░░░                                                             "
 echo -e "[+] Make \e[31mCritical\e[0m great again"
 echo
 
@@ -21,21 +28,24 @@ ORG=$1
 LOWER=$(echo "${ORG}" | tr '[:upper:]' '[:lower:]')
 bash ghClone.sh $ORG
 bash main.sh $ORG "/tmp/$ORG/REPOS/${LOWER}"
-bash depChecker.sh $ORG "/tmp/$ORG"
+bash depChecker.sh $ORG "/tmp/$ORG/REPOS"
+
+echo -e "${GREEN}[-]Scanning with Trufflehog : [-]${RESET}$NAME"
 trufflehog github --only-verified --token=$GITHUB_TOKEN --issue-comments --pr-comments --gist-comments --include-members --archive-max-depth=50 --org=$ORG
 
-echo -e "${BLUE}Scanning Secrets from Javascripts $NAME ${NC}"
+echo -e "${GREEN}[-]Scanning Secrets : [-]${RESET}$NAME"
 find "/tmp/$ORG/REPOS/${LOWER}" -name "*.js" | xargs -I {} bash JsLeak.sh {} | sort -u | anew "/tmp/$ORG/secrets.potential"
 
 while IFS= read -r member; do
-bash main.sh ${member} "/tmp/$ORG/${member}/"
-bash depChecker.sh ${member} "/tmp/$ORG/${member}"
-
 LOWER=$(echo "${member}" | tr '[:upper:]' '[:lower:]')
+bash main.sh ${member} "/tmp/$ORG/${member}/REPOS/${LOWER}"
+bash depChecker.sh ${member} "/tmp/$ORG/${member}/REPOS"
+
 for f in "/tmp/$ORG/${member}/REPOS/${LOWER}/*"; do
   [ -e "$f" ] || continue
+  echo -e "${GREEN}[-]Scanning Secrets : [-]${RESET}${member}"
   trufflehog git file://"/tmp/$ORG/${member}/REPOS/${LOWER}/${f##*/}" --only-verified --archive-max-depth=150
-  echo -e "${BLUE}Scanning Secrets from Javascripts $NAME ${NC}"
+  echo -e "${GREEN}[-]Scanning Secrets : [-]${RESET}${member}"
 find "/tmp/$ORG/${member}/REPOS/${LOWER}/" -name "*.js" | xargs -I {} bash JsLeak.sh {} | sort -u | anew "/tmp/$ORG/${member}/secrets.potential"
 done
 done < "/tmp/$ORG/member.usernames"
