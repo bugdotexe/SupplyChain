040af06525b9d642a6347e3ff73842a5e9200429
Nyein Chan Aung <nca0x93@gmail.com>
2025-10-19 18:09:11 +0630

    Update depChecker.sh

--- depChecker.sh
+++ depChecker.sh
@@ -23,6 +23,8 @@ cat $OUT_PUT/gem.checked | grep "is available" | anew $OUT_PUT/gem.potential
 broken_github() {
 echo -e "${BLUE}Scanning Broken GITHUB link $NAME ${NC}"
 cat $OUT_PUT/github.account | cut -d "/" -f1,2,3,4 | sort | uniq | xargs -I {} sh scan-broken.sh {} | anew $OUT_PUT/github.potential
+
+echo -e "${BLUE}Scanning Broken GITHUB Action $NAME ${NC}"
 cat $OUT_PUT/github.action | cut -d "/" -f1,2,3,4 | sort | uniq | xargs -I {} sh scan-broken.sh {} | anew $OUT_PUT/github.potential
 
 #cat $OUT_PUT/github.account | grep -v ajax.googleapis.com\|awscli.amazonaws.com\|docs.aws.amazon.com\|ec2.amazonaws.com\|fonts.googleapis.com\|maps.googleapis.com\|oauth2.googleapis.com\|openidconnect.googleapis.com\|play.googleapis.com\|sns.amazonaws.com\|go-integration-test" | grep "actions-contrib\|googleapis\|amazonaws\|vercel.app\|netlify\|herokuapp\|surge.sh\|now.sh\|plugins.svn.wordpress.org\|npmjs.org\/package"
