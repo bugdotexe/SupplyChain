3ad087ec5488dac1e3c00df490bfac126b54806a
Nyein Chan Aung <nca0x93@gmail.com>
2025-10-19 18:08:22 +0630

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -33,19 +33,8 @@ bash depChecker.sh $ORG "/tmp/$ORG/REPOS"
 echo -e "${GREEN}[-]Scanning with Trufflehog : [-]${RESET}$NAME"
 trufflehog github --only-verified --token=$GITHUB_TOKEN --issue-comments --pr-comments --gist-comments --include-members --archive-max-depth=50 --org=$ORG
 
-echo -e "${GREEN}[-]Scanning Secrets : [-]${RESET}$NAME"
-find "/tmp/$ORG/REPOS/${LOWER}" -name "*.js" | xargs -I {} bash JsLeak.sh {} | sort -u | anew "/tmp/$ORG/secrets.potential"
-
 while IFS= read -r member; do
 LOWER=$(echo "${member}" | tr '[:upper:]' '[:lower:]')
 bash main.sh ${member} "/tmp/$ORG/${member}/REPOS/${LOWER}"
 bash depChecker.sh ${member} "/tmp/$ORG/${member}/REPOS"
-
-for f in "/tmp/$ORG/${member}/REPOS/${LOWER}/*"; do
-  [ -e "$f" ] || continue
-  echo -e "${GREEN}[-]Scanning Secrets : [-]${RESET}${member}"
-  trufflehog git file://"/tmp/$ORG/${member}/REPOS/${LOWER}/${f##*/}" --only-verified --archive-max-depth=150
-  echo -e "${GREEN}[-]Scanning Secrets : [-]${RESET}${member}"
-find "/tmp/$ORG/${member}/REPOS/${LOWER}/" -name "*.js" | xargs -I {} bash JsLeak.sh {} | sort -u | anew "/tmp/$ORG/${member}/secrets.potential"
-done
 done < "/tmp/$ORG/member.usernames"
